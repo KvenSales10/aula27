8a744bda4ea65db2436278c0562a0f6948b9f3be
KevenSales10 <kelven.morais@aluno.senai.br>
2024-12-11 10:05:18 -0500

    horarios do dia

--- funcao.py
+++ funcao.py
@@ -1,13 +1,15 @@
-#declarar uma finçao 
-def bom_dia():
-    print("BOM DAI!")
+def saudacao (hora_do_dia ):
+    #dar bom dia 
+    if (hora_do_dia >= 0) and (hora_do_dia <= 12):
+        print ("bom dia  !!!")
+    elif (hora_do_dia>=13) and (hora_do_dia<= 18):
+          print ("boa tarde !!!")
+    elif (hora_do_dia>=19) and (hora_do_dia<= 23):
+          print ("boa noite !!!") 
+    else:
+        print("esse horario nao existe ")
+ #fora daq funçao 
+#peço  para o usuario o usuario dizer a hora do dia 
+responda = int (input("digite que horas sao "))
 
-def BOA_NOITE():
-    print("BOM NOITE!")
-
-def BOA_TERDE ():
-    print("BOM TARDE!")
-
-bom_dia ()
-BOA_NOITE ()
-BOA_TERDE()
+saudacao(responda) 
