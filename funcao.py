527a148db86f60077e9b71b3dba3bf381eb16d68
KevenSales10 <kelven.morais@aluno.senai.br>
2024-12-11 08:46:57 -0500

    aprendendo a fzer finçao ]

---
+++ funcao.py
@@ -0,0 +1,13 @@
+#declarar uma finçao 
+def bom_dia():
+    print("BOM DAI!")
+
+def BOA_NOITE():
+    print("BOM NOITE!")
+
+def BOA_TERDE ():
+    print("BOM TARDE!")
+
+bom_dia ()
+BOA_NOITE ()
+BOA_TERDE()
